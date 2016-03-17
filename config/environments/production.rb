ed23cb381bc7f8b23a4654e49219d27687ebb0c2
Sean Carroll <sfcarroll@gmail.com>
2016-03-17 14:27:17 +0100

    fixes hosting institution error on create

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'crowdai-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'crowdai-stg.herokuapp.com' }
 
-  config.action_mailer.raise_delivery_errors = false
 
 end

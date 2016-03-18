431de61652ba8277e78b2384eb3267e08b0c3be8
Sean Carroll <sfcarroll@gmail.com>
2016-03-18 16:03:02 +0100

    navbar

---
+++ app/helpers/font_awesome_helper.rb
@@ -0,0 +1,19 @@
+module FontAwesomeHelper
+
+  def awesome_link(icon_class = nil, link_name = nil, url_options = nil, html_options = nil, &block)
+    link_to("#{ icon_tag(icon_class) } #{link_name}".html_safe, url_options, html_options, &block)
+  end
+
+  private
+  def icon_tag(icon_class)
+    "<i class='fa #{ icon_class }'></i>"
+  end
+
+# usage:
+#  <%= awesome_link 'fa-arrow-circle-o-up', 'wikipedia', 'www.wikipedia.com' %>
+#  <%= awesome_link('fa-minus-circle', travel_path(current_island.name, travel) %>
+#  <li><%= awesome_link('fa-bookmark',
+#                     sentence_unbookmark_path(dialog, current_user.id),
+#                     method: :delete, remote: true, title: "Remove bookmark") %></li>
+
+end

2e761475904ae566f05cca073a2b5b47d5edcee9
Sean Carroll <sfcarroll@gmail.com>
2016-03-22 15:42:33 +0100

    assets cleanup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb','*.scss']
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.

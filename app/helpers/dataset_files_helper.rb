461c981272512eb19fd58596cff81ddfda165dd9
Sean Carroll <sfcarroll@gmail.com>
2016-04-11 19:21:29 +0200

    file size

--- app/helpers/dataset_files_helper.rb
+++ app/helpers/dataset_files_helper.rb
@@ -1,6 +1,6 @@
 module DatasetFilesHelper
   # TODO refactor see also sub_file_info
   def file_info(file)
-    "#{file.dataset_file_file_name} (#{number_to_human_size(1024 * file.dataset_file_file_size)})"
+    "#{file.dataset_file_file_name} (#{number_to_human_size(file.dataset_file_file_size)})"
   end
 end

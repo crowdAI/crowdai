95b491620ca8eb9a531b78c932032adee5d57121
Sean Carroll <sfcarroll@gmail.com>
2016-04-12 11:01:19 +0200

    www.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'crowdai.org' }
+  config.action_mailer.default_url_options = { host: 'www.crowdai.org' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true

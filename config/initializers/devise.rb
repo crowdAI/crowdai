abcee3d234f592b653874c04cf206b7d3e1a8d67
Sean Carroll <sfcarroll@gmail.com>
2016-04-12 10:13:56 +0200

    devise secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,6 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
   # config.secret_key = 'db0a8dde7cb9a4fadec8c39c5cbaf284d8856e03884ae11737d465d6fc295273254b8f0c0e7ebe8128ea35032d58857504c22318817fe56fb9156910a5abe73d'
 
   # ==> Mailer Configuration
@@ -129,7 +130,7 @@ Devise.setup do |config|
   config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
-  # config.confirmation_keys = [:email]
+  config.confirmation_keys = [:email]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.

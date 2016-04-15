c7ccd2ab9cc7014d081196273204126cc0e75991
Sean Carroll <sfcarroll@gmail.com>
2016-04-15 14:52:44 +0200

    grading api

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BaseController < ApplicationController
-  protect_from_forgery with: :null_session
+  #protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   respond_to :json
 end
--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -1,13 +1,21 @@
 class Api::V1::SubmissionsController < Api::V1::BaseController
+  skip_filter :authenticate_participant!
   before_action :set_submission
 
   def update
+    @submission.update({score: params[:score], score_secondary: params[:score_secondary]})
+    if @submission.save!
+      render json: {message: "submission #{@submission.id} scored"}, status: :accepted
+    else
+      render json: {message: "submission #{@submission.id} errored"}, status: :internal_server_error
+    end
   end
 
   private
   def submissions_params
     params.require(:submission).permit(:id,:score,:score_secondary)
   end
+
   def set_submission
     @submission = Submission.find(params[:id])
   end

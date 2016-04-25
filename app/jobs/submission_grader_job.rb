ea8145d651b4a7ab55850d84d56ec6b7bc3e6792
Sean Carroll <sfcarroll@gmail.com>
2016-04-25 14:26:27 +0200

    ~bugfix on grading program

--- app/jobs/submission_grader_job.rb
+++ app/jobs/submission_grader_job.rb
@@ -2,7 +2,6 @@ class SubmissionGraderJob < BaseJob
   queue_as :default
 
   def perform(*args)
-    @logger.info("grader running for: #{args[0]}")
     Grader.new.grade(args[0][:submission_id])
   end
 

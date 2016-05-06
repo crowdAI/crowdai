521d0582dd934f1d5f0a87b1d1f5eb4bf2c215c0
Sean Carroll <sfcarroll@gmail.com>
2016-05-06 11:25:00 +0200

    post specs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :participant
+
+  validates :topic, presence: true
+  validates :participant, presence: true
 end
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -9,6 +9,7 @@ RSpec.feature "topic", type: :feature do
     @admin_participant = create :participant, :admin
     @challenge = create :challenge, :with_events
     @login_page = DeviseSessionsNew.new
+    @running_challenge = create :challenge, :with_events
   end
 
 
@@ -32,18 +33,16 @@ RSpec.feature "topic", type: :feature do
 
     scenario "a participant can respond to a post" do
       @login_page.visit_page.login(@participant)
-      topics = TopicsPages.new
-      topics.index(@challenge)
-      click_link "+ New Topic"
-      fill_in "Topic", with: "Post test"
-      click_button "Create Topic"
-      expect(page).to have_content("Topic was successfully created.")
-      click_link "Post test"
-      expect(page).to have_content("Post test")
-      find('#markdown_textarea').set('### Markdown Heading')
+      @topic = create :topic, :with_posts
+      @topic.challenge = @running_challenge
+      @topic.save
+      visit new_topic_post_path(@topic)
+      expect(page).to have_content(@topic.topic)
+      find('#markdown_textarea').set('## Post Reply Test')
       click_button "Create Post"
-      expect(page).to have_selector("h3", text: "Markdown Heading")
-      expect(page).to_not have_content("Post was successfully created.")
+      expect(page).to have_selector("h2", text: "Post Reply Test")
+      expect(page).to have_content("Post was successfully created.")
+
     end
   end
 

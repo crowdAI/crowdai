4ed48fe1fd35efb6dc3040fcd6dfce89b570de9a
Sean Carroll <sfcarroll@gmail.com>
2016-05-10 11:23:43 +0200

    timelines refactor

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -11,7 +11,7 @@ class SubmissionsController < ApplicationController
   end
 
   def show
-      logger.info("logging")
+    @timeline = @challenge.timeline
     if @submission.participant_id != current_participant.id && !current_participant.admin?
         logger.info("redirecting")
       redirect_to '/', notice: "You don't have permission for this action."

1ab70328ae9eda13462fc9955b1efb46a2b5721f
Sean Carroll <sfcarroll@gmail.com>
2016-05-12 10:00:29 +0200

    user online indicator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  after_filter :participant_activity
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
@@ -8,5 +9,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :name, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+  end
+
+  private
+  def participant_activity
+    current_participant.try :touch
   end
 end

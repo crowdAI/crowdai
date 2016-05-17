5b58f3d02d03a79630278c66d1ea27682c24d1f5
Sean Carroll <sfcarroll@gmail.com>
2016-05-17 10:48:56 +0200

    reconfirmation job

--- app/jobs/add_to_mail_chimp_list_job.rb
+++ app/jobs/add_to_mail_chimp_list_job.rb
@@ -6,8 +6,9 @@ class AddToMailChimpListJob < ActiveJob::Base
       participant = Participant.find(participant_id)
       gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
       gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: participant.email,
-                                                                   status: 'subscribed',
-                                                                   merge_fields: {FNAME: participant.name }})
+                                                                    status: 'subscribed',
+                                                                    merge_fields: {FNAME: participant.name }})
+      end
     end
   end
 end

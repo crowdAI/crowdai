369c85fc11dd8063f6e72a7faf4d762e710433b5
Sean Carroll <sfcarroll@gmail.com>
2016-05-27 00:48:30 +0200

    create challenge feature specs

--- spec/features/challenge_admin_spec.rb
+++ spec/features/challenge_admin_spec.rb
@@ -1,28 +1,55 @@
 require "rails_helper"
 
 feature 'Challenge CRUD for admin user', js: true do
+
   let!(:admin) { create(:participant, :admin) }
   let!(:organizer) { create(:organizer) }
   let!(:challenge_data) { build(:challenge) }
 
   describe "create new challenge page" do
     before(:example) do
-      visit_organizer(admin,organizer)
+      #visit_organizer(admin,organizer)  TODO
+      visit_landing_page(admin  )
+      visit organizer_path(organizer)
       click_link '+ New Challenge'
     end
 
-    scenario 'Overview page' do
-      click_link 'Overview'
-      fill_in 'Challenge',          with: challenge_data.challenge
-      fill_in 'Tagline',            with: challenge_data.tagline
-      select organizer.organizer,   from: 'Organizer'
-      select 'Draft',               from: 'Status'
-      click_button 'Create Challenge'
-      expect(page)
+
+    scenario 'create perpetual challenge' do
+      create_perpetual_challenge(challenge_data)
+      expect(page).to have_content "Challenge was successfully created."
     end
+
+
+    scenario 'create challenge with start and finish dates' do
+      enter_challenge_without_timeline(challenge_data)
+
+      find('#timeline-tab').click
+      click_link 'Add Milestone'
+      fill_in 'Seq',          with: '0'
+      fill_in 'Event',        with: 'Start of event'
+
+      click_link 'Add Milestone'
+      fill_in 'Seq',          with: '1'
+      fill_in 'Event',        with: 'End of event'
+
+      click_button 'Create Challenge'
+      expect(page).to have_content "Challenge was successfully created."
+    end
+
+
+    scenario 'edit challenge after creation' do
+      create_perpetual_challenge(challenge_data)
+      click_link 'Edit'
+      expect(page).to have_content 'Editing Challenge'
+    end
+
+
+    scenario 'create challenge with dataset' do
+      skip("spec to be coded")
+    end
+
   end
-
-
 
 
 end

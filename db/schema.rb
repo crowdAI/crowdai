4f5caada4c5cfd05fa17ada503c84771c8af75d9
Sean Carroll <sfcarroll@gmail.com>
2016-06-02 15:56:11 +0200

    remove migration

--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,58 @@ ActiveRecord::Schema.define(version: 20160602130102) do
   add_index "submissions", ["challenge_id"], :name=>"index_submissions_on_challenge_id", :using=>:btree
   add_index "submissions", ["participant_id"], :name=>"index_submissions_on_participant_id", :using=>:btree
 
-
+  create_view "challenge_views", <<-'END_VIEW_CHALLENGE_VIEWS', :force => true
+SELECT c.id,
+    c.organizer_id,
+    c.challenge,
+    c.status_cd,
+    c.description,
+    c.evaluation_markdown,
+    c.rules,
+    c.prizes,
+    c.resources,
+    c.created_at,
+    c.updated_at,
+    c.dataset_description,
+    c.submission_instructions,
+    c.tagline,
+    c.evaluation,
+    c.primary_sort_order_cd,
+    c.secondary_sort_order_cd,
+    c.description_markdown,
+    c.rules_markdown,
+    c.prizes_markdown,
+    c.resources_markdown,
+    c.dataset_description_markdown,
+    c.submission_instructions_markdown,
+    c.perpetual_challenge,
+    c.grading_factor,
+    c.grader_cd,
+    c.answer_file_s3_key,
+    c.page_views,
+    ((count(s.*) / 2))::integer AS submission_count,
+    count(cp.*) AS participant_count
+   FROM ((challenges c
+     LEFT JOIN submissions s ON ((s.challenge_id = c.id)))
+     LEFT JOIN ( SELECT c_1.id,
+            c_1.id AS challenge_id,
+            p.id AS participant_id
+           FROM challenges c_1,
+            participants p,
+            submissions s_1
+          WHERE ((s_1.challenge_id = c_1.id) AND (s_1.participant_id = p.id))
+        UNION
+         SELECT c_1.id,
+            c_1.id AS challenge_id,
+            p.id AS participant_id
+           FROM challenges c_1,
+            participants p,
+            topics t
+          WHERE ((t.challenge_id = c_1.id) AND ((t.participant_id = p.id) OR (EXISTS ( SELECT 'X'
+                   FROM posts ps
+                  WHERE ((ps.topic_id = t.id) AND (ps.participant_id = p.id))))))) cp ON ((cp.id = c.id)))
+  GROUP BY c.id, c.page_views
+  END_VIEW_CHALLENGE_VIEWS
 
   create_table "dataset_file_downloads", force: :cascade do |t|
     t.integer  "participant_id"
@@ -123,7 +174,38 @@ ActiveRecord::Schema.define(version: 20160602130102) do
   end
   add_index "images", ["imageable_type", "imageable_id"], :name=>"index_images_on_imageable_type_and_imageable_id", :using=>:btree
 
-
+  create_view "leaderboards", <<-'END_VIEW_LEADERBOARDS', :force => true
+SELECT l.row_num,
+    l.id,
+    l.challenge_id,
+    l.participant_id,
+    l.name,
+    l.entries,
+    l.score,
+    l.score_secondary,
+    l.created_at,
+    l.updated_at
+   FROM ( SELECT row_number() OVER (PARTITION BY s.challenge_id, s.participant_id ORDER BY s.score DESC, s.score_secondary) AS row_num,
+            s.id,
+            s.challenge_id,
+            s.participant_id,
+            p.name,
+            cnt.entries,
+            s.score,
+            s.score_secondary,
+            s.created_at,
+            s.updated_at
+           FROM submissions s,
+            participants p,
+            ( SELECT c.challenge_id,
+                    c.participant_id,
+                    count(c.*) AS entries
+                   FROM submissions c
+                  GROUP BY c.challenge_id, c.participant_id) cnt
+          WHERE ((p.id = s.participant_id) AND ((s.grading_status_cd)::text = 'graded'::text) AND (cnt.challenge_id = s.challenge_id) AND (cnt.participant_id = s.participant_id))) l
+  WHERE (l.row_num = 1)
+  ORDER BY l.score DESC, l.score_secondary
+  END_VIEW_LEADERBOARDS
 
   create_table "organizers", force: :cascade do |t|
     t.string   "organizer"
@@ -134,6 +216,53 @@ ActiveRecord::Schema.define(version: 20160602130102) do
     t.boolean  "approved",    :default=>false
   end
 
+  create_view "participant_challenges", <<-'END_VIEW_PARTICIPANT_CHALLENGES', :force => true
+SELECT c.id,
+    c.id AS challenge_id,
+    p.id AS participant_id,
+    c.organizer_id,
+    c.challenge,
+    c.description,
+    c.rules,
+    c.prizes,
+    c.resources,
+    c.tagline,
+    p.name,
+    p.email,
+    p.last_sign_in_at,
+    p.bio,
+    p.github,
+    p.linkedin,
+    p.twitter
+   FROM challenges c,
+    participants p,
+    submissions s
+  WHERE ((s.challenge_id = c.id) AND (s.participant_id = p.id))
+UNION
+ SELECT c.id,
+    c.id AS challenge_id,
+    p.id AS participant_id,
+    c.organizer_id,
+    c.challenge,
+    c.description,
+    c.rules,
+    c.prizes,
+    c.resources,
+    c.tagline,
+    p.name,
+    p.email,
+    p.last_sign_in_at,
+    p.bio,
+    p.github,
+    p.linkedin,
+    p.twitter
+   FROM challenges c,
+    participants p,
+    topics t
+  WHERE ((t.challenge_id = c.id) AND ((t.participant_id = p.id) OR (EXISTS ( SELECT 'X'
+           FROM posts ps
+          WHERE ((ps.topic_id = t.id) AND (ps.participant_id = p.id))))))
+  END_VIEW_PARTICIPANT_CHALLENGES
 
   create_table "participants", force: :cascade do |t|
     t.string   "email",                  :default=>"", :null=>false
@@ -245,140 +374,4 @@ ActiveRecord::Schema.define(version: 20160602130102) do
   add_foreign_key "topics", "participants"
   add_foreign_key "tutorials", "participants"
   add_foreign_key "votes", "participants"
-
-
-  create_view "challenge_views", <<-'END_VIEW_CHALLENGE_VIEWS', :force => true
-SELECT c.id,
-    c.organizer_id,
-    c.challenge,
-    c.status_cd,
-    c.description,
-    c.evaluation_markdown,
-    c.rules,
-    c.prizes,
-    c.resources,
-    c.created_at,
-    c.updated_at,
-    c.dataset_description,
-    c.submission_instructions,
-    c.tagline,
-    c.evaluation,
-    c.primary_sort_order_cd,
-    c.secondary_sort_order_cd,
-    c.description_markdown,
-    c.rules_markdown,
-    c.prizes_markdown,
-    c.resources_markdown,
-    c.dataset_description_markdown,
-    c.submission_instructions_markdown,
-    c.perpetual_challenge,
-    c.grading_factor,
-    c.grader_cd,
-    c.answer_file_s3_key,
-    c.page_views,
-    ((count(s.*) / 2))::integer AS submission_count,
-    count(cp.*) AS participant_count
-   FROM ((challenges c
-     LEFT JOIN submissions s ON ((s.challenge_id = c.id)))
-     LEFT JOIN ( SELECT c_1.id,
-            c_1.id AS challenge_id,
-            p.id AS participant_id
-           FROM challenges c_1,
-            participants p,
-            submissions s_1
-          WHERE ((s_1.challenge_id = c_1.id) AND (s_1.participant_id = p.id))
-        UNION
-         SELECT c_1.id,
-            c_1.id AS challenge_id,
-            p.id AS participant_id
-           FROM challenges c_1,
-            participants p,
-            topics t
-          WHERE ((t.challenge_id = c_1.id) AND ((t.participant_id = p.id) OR (EXISTS ( SELECT 'X'
-                   FROM posts ps
-                  WHERE ((ps.topic_id = t.id) AND (ps.participant_id = p.id))))))) cp ON ((cp.id = c.id)))
-  GROUP BY c.id, c.page_views
-  END_VIEW_CHALLENGE_VIEWS
-
-  create_view "leaderboards", <<-'END_VIEW_LEADERBOARDS', :force => true
-SELECT l.row_num,
-    l.id,
-    l.challenge_id,
-    l.participant_id,
-    l.name,
-    l.entries,
-    l.score,
-    l.score_secondary,
-    l.created_at,
-    l.updated_at
-   FROM ( SELECT row_number() OVER (PARTITION BY s.challenge_id, s.participant_id ORDER BY s.score DESC, s.score_secondary) AS row_num,
-            s.id,
-            s.challenge_id,
-            s.participant_id,
-            p.name,
-            cnt.entries,
-            s.score,
-            s.score_secondary,
-            s.created_at,
-            s.updated_at
-           FROM submissions s,
-            participants p,
-            ( SELECT c.challenge_id,
-                    c.participant_id,
-                    count(c.*) AS entries
-                   FROM submissions c
-                  GROUP BY c.challenge_id, c.participant_id) cnt
-          WHERE ((p.id = s.participant_id) AND ((s.grading_status_cd)::text = 'graded'::text) AND (cnt.challenge_id = s.challenge_id) AND (cnt.participant_id = s.participant_id))) l
-  WHERE (l.row_num = 1)
-  ORDER BY l.score DESC, l.score_secondary
-  END_VIEW_LEADERBOARDS
-
-
-    create_view "participant_challenges", <<-'END_VIEW_PARTICIPANT_CHALLENGES', :force => true
-  SELECT c.id,
-      c.id AS challenge_id,
-      p.id AS participant_id,
-      c.organizer_id,
-      c.challenge,
-      c.description,
-      c.rules,
-      c.prizes,
-      c.resources,
-      c.tagline,
-      p.name,
-      p.email,
-      p.last_sign_in_at,
-      p.bio,
-      p.github,
-      p.linkedin,
-      p.twitter
-     FROM challenges c,
-      participants p,
-      submissions s
-    WHERE ((s.challenge_id = c.id) AND (s.participant_id = p.id))
-  UNION
-   SELECT c.id,
-      c.id AS challenge_id,
-      p.id AS participant_id,
-      c.organizer_id,
-      c.challenge,
-      c.description,
-      c.rules,
-      c.prizes,
-      c.resources,
-      c.tagline,
-      p.name,
-      p.email,
-      p.last_sign_in_at,
-      p.bio,
-      p.github,
-      p.linkedin,
-      p.twitter
-     FROM challenges c,
-      participants p,
-      topics t
-    WHERE ((t.challenge_id = c.id) AND ((t.participant_id = p.id) OR (EXISTS ( SELECT 'X'
-             FROM posts ps
-            WHERE ((ps.topic_id = t.id) AND (ps.participant_id = p.id))))))
-    END_VIEW_PARTICIPANT_CHALLENGES
 end

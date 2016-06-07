e65fa7761ce4e42e8e39a286433317cf854d6310
Sean Carroll <sfcarroll@gmail.com>
2016-06-07 12:27:20 +0200

    calender available on mobile

--- app/assets/javascripts/controllers/challenges_controller.js
+++ app/assets/javascripts/controllers/challenges_controller.js
@@ -14,22 +14,35 @@ $(document).on('ready page:load', function() {
 
   }
 
+
   if (!_.isEmpty(gon) && gon.rails.controller == 'challenges' && gon.rails.action == 'show') {
 
-    function adjustProgress() {
-      var percent   = gon.percent_progress;
+    function adjustProgress(percent) {
+      $('.ai-progressbar').width(percent + '%');
+    }
+
+
+    function adjustText(percent) {
       var ai_red    = '#FF4E48';           // see also _variables.scss
       var ai_white  = '#FFFFFF';
-      $('.ai-progressbar').width(percent + '%');
-      if(percent < 60) {
-        $('.ai-progress-percent').css({'color': ai_red, 'width': percent + '%', 'left': percent + 5 + '%'});
-      }
-      if(percent >= 60) {
-        $('.ai-progress-percent').css({'color': ai_white, 'width': percent + '%'});
+      var screen_width = $(window).width();
+      console.log(screen_width);
+      console.log(percent);
+      if(screen_width < 690) {
+        $('.ai-progress-text').css('visibility','hidden');
+      } else {
+        if(percent < 60) {
+          $('.ai-progress-text').css({'color': ai_red, 'width': percent + '%', 'left': percent + 5 + '%'});
+        }
+        if(percent >= 60) {
+          $('.ai-progress-text').css({'color': ai_white, 'width': percent + '%'});
+        }
       }
     }
 
-    adjustProgress(); 
+
+    adjustProgress(gon.percent_progress);
+    adjustText(gon.percent_progress);
   }
 
 });

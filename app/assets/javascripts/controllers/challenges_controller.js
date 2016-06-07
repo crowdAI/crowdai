8c5a17920405c35d531c838e84e106ee16066ee3
Sean Carroll <sfcarroll@gmail.com>
2016-06-07 11:22:39 +0200

    adjust progress bar with js

--- app/assets/javascripts/controllers/challenges_controller.js
+++ app/assets/javascripts/controllers/challenges_controller.js
@@ -1,7 +1,6 @@
 $(document).on('ready page:load', function() {
   if (!_.isEmpty(gon) && gon.rails.controller == 'challenges' && gon.rails.action == 'edit') {
 
-
     function toggleEvents() {
       if ( $('#challenge_perpetual_challenge').is(':checked')){
         $('.events-configuration').hide();
@@ -14,4 +13,23 @@ $(document).on('ready page:load', function() {
     $('#challenge_perpetual_challenge')[0].addEventListener('click',toggleEvents);  // user action
 
   }
+
+  if (!_.isEmpty(gon) && gon.rails.controller == 'challenges' && gon.rails.action == 'show') {
+
+    function adjustProgress() {
+      var percent   = gon.percent_progress;
+      var ai_red    = '#FF4E48';           // see also _variables.scss
+      var ai_white  = '#FFFFFF';
+      $('.ai-progressbar').width(percent + '%');
+      if(percent < 60) {
+        $('.ai-progress-percent').css({'color': ai_red, 'width': percent + '%', 'left': percent + 5 + '%'});
+      }
+      if(percent >= 60) {
+        $('.ai-progress-percent').css({'color': ai_white, 'width': percent + '%'});
+      }
+    }
+
+    adjustProgress(); 
+  }
+
 });

c464f4c7887252c1e57992c66c738f5580487ce9
Sean Carroll <sfcarroll@gmail.com>
2016-06-09 11:15:20 +0200

    database refresh script

--- app/jobs/add_to_mail_chimp_list_job.rb
+++ app/jobs/add_to_mail_chimp_list_job.rb
@@ -2,16 +2,14 @@ class AddToMailChimpListJob < ActiveJob::Base
   queue_as :default
 
   def perform(participant_id)
-    if Rails.env.production?
-      participant = Participant.find(participant_id)
-      lower_case_md5_hashed_email_address = Digest::MD5.hexdigest(participant.email.downcase)
-      gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
-      resp = gibbon.lists(ENV['MAILCHIMP_LIST_ID'])
-                    .members(lower_case_md5_hashed_email_address)
-                    .upsert(body: {email_address: participant.email,
-                                          status: 'subscribed',
-                                    merge_fields: { FNAME: participant.name }})
-      logger.info("AddToMailChimpListJob API response: #{resp}")
-    end
+    participant = Participant.find(participant_id)
+    lower_case_md5_hashed_email_address = Digest::MD5.hexdigest(participant.email.downcase)
+    gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+    resp = gibbon.lists(ENV['MAILCHIMP_LIST_ID'])
+                 .members(lower_case_md5_hashed_email_address)
+                 .upsert(body: {email_address: participant.email,
+                                       status: 'subscribed',
+                                 merge_fields: { FNAME: participant.name }})
+    logger.info("AddToMailChimpListJob API response: #{resp}")
   end
 end

d2183fd3db2049357bb16f6f8750b86eb65766c0
Sean Carroll <sfcarroll@gmail.com>
2016-06-09 10:16:56 +0200

    change mailchimp list addition to upsert

--- app/jobs/add_to_mail_chimp_list_job.rb
+++ app/jobs/add_to_mail_chimp_list_job.rb
@@ -4,11 +4,14 @@ class AddToMailChimpListJob < ActiveJob::Base
   def perform(participant_id)
     if Rails.env.production?
       participant = Participant.find(participant_id)
+      lower_case_md5_hashed_email_address = Digest::MD5.hexdigest(participant.email.downcase)
       gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
-      resp = gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: participant.email,
-                                                                    status: 'subscribed',
-                                                                    merge_fields: {FNAME: participant.name }})
-      logger.info(resp)
+      resp = gibbon.lists(ENV['MAILCHIMP_LIST_ID'])
+                    .members(lower_case_md5_hashed_email_address)
+                    .upsert(body: {email_address: participant.email,
+                                          status: 'subscribed',
+                                    merge_fields: { FNAME: participant.name }})
+      logger.info("AddToMailChimpListJob API response: #{resp}")
     end
   end
 end

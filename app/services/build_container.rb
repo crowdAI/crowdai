0a0dc254d911c4dab5d22998bcfb1d8e943797e1
Sean Carroll <sfcarroll@gmail.com>
2016-08-14 12:38:51 +0200

    docker create

--- app/services/build_container.rb
+++ app/services/build_container.rb
@@ -3,7 +3,11 @@ class BuildContainer
   def run(configuration_id)
     config = DockerConfiguration.find(configuration_id)
     image = Docker::Image.create('fromImage' => 'busybox')
+    puts "created image #{image.inspect}"
     image.insert_local('localPath' => 'scripts/docker/callback.sh', 'outputPath' => config.mount_point)
+    puts "inserted files"
+    container = Docker::Container.create('Cmd' => ['bash'], 'Image' => image)
+    puts 'image created'
   end
 
 end

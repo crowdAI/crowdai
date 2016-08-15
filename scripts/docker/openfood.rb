cda90991b0be04744766a3d9872bbeeed80dbc18
Sean Carroll <sfcarroll@gmail.com>
2016-08-15 16:24:09 +0200

    docker configuration

---
+++ scripts/docker/openfood.rb
@@ -0,0 +1,12 @@
+
+header = 'product_id,nutrition_id,per_hundred,per_portion,percent\n'
+
+File.open('testfile.csv', 'w') do |f|
+  f.write(header)
+  (7000..7005).each do |i|
+    (1..16).each do |j|
+      line = "#{i},#{j},-1,-1,-1\n"
+      f.write(line)
+    end
+  end
+end

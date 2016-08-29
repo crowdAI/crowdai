a2e07331a6b0dbc7c575ec7d9f0e4c2e5181e48b
Sean Carroll <sfcarroll@gmail.com>
2016-08-29 12:14:10 +0200

    test cleanup wip

--- spec/helpers/challenges_helper_spec.rb
+++ spec/helpers/challenges_helper_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-describe ArticlesHelper do
+describe ChallengesHelper do
 
 end
--- spec/helpers/font_awesome_helper_spec.rb
+++ spec/helpers/font_awesome_helper_spec.rb
@@ -1,10 +1,8 @@
-# -*- encoding: utf-8 -*-
-
 require 'rails_helper'
 
 describe FontAwesomeHelper do
 
-  
+=begin
   describe '#awesome_link' do
     it 'works' do
       result = awesome_link(icon_class, link_name, url_options, html_options, &block)
@@ -12,7 +10,7 @@ describe FontAwesomeHelper do
     end
   end
 
-  
+
   describe '#awesome_icon_tag' do
     it 'works' do
       result = awesome_icon_tag(icon_class)
@@ -20,12 +18,13 @@ describe FontAwesomeHelper do
     end
   end
 
-  
+
   describe '#:delete,' do
     it 'works' do
       result = :delete,
       expect(result).not_to be_nil
     end
   end
+=end
 
 end

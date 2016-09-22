f78a8bcbc7af7bea0a387b8bf2631331271b0809
Sean Carroll <sfcarroll@gmail.com>
2016-09-22 15:51:09 +0200

    Administrate support for friendly id

--- app/controllers/admin/challenges_controller.rb
+++ app/controllers/admin/challenges_controller.rb
@@ -1,19 +1,8 @@
 module Admin
   class ChallengesController < Admin::ApplicationController
-    # To customize the behavior of this controller,
-    # simply overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = Challenge.all.paginate(10, params[:page])
-    # end
 
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   Challenge.find_by!(slug: param)
-    # end
-
-    # See https://administrate-docs.herokuapp.com/customizing_controller_actions
-    # for more information
+    def find_resource(param)
+      Challenge.find_by!(slug: param)
+    end
   end
 end

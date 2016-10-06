057c2409c348a20fbcfd865c168e93d9fbc84a50
Sean Carroll <sfcarroll@gmail.com>
2016-10-06 10:29:53 +0200

    moved scripts to private repo

--- scripts/challenges/openfood/semicolonify.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env ruby
-puts File.read(ARGV[0]).split("\n").delete_if{|line| line =~ /^\s*#/}.compact.join(';')
--- scripts/challenges/openfood/test.rb
+++
@@ -1,2 +0,0 @@
-image = Image.find(4305)
-puts "wget --output-document image-#{image.product_id}.jpg #{image.data.url(:xlarge)} "

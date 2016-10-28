69732e87508747a1e35bd2b329a6b34d5f6cb0b1
S.P. Mohanty <spmohanty91@gmail.com>
2016-10-28 14:18:09 +0100

    MozFest cleanup....Yayyyyyyy

--- scripts/docker/callback.sh
+++ scripts/docker/callback.sh
@@ -2,4 +2,4 @@
 hostname="`eval cat /etc/hostname`"
 echo $hostname
 set -x
-curl -X POST -G https://crowdai-stg.herokuapp.com/api/v1/docker_callbacks -d "container=$hostname" -d "score=999999" -d "score_secondary=123456" -H 'Authorization: Token token="***REMOVED***"'
+curl -X POST -G https://crowdai-stg.herokuapp.com/api/v1/docker_callbacks -d "container=$hostname" -d "score=999999" -d "score_secondary=123456" -H 'Authorization: Token token="REDACTED"'
--- scripts/docker/rails_api.py
+++ scripts/docker/rails_api.py
@@ -2,7 +2,7 @@ import requests
 
 class RailsAPI(object):
   def __init__(self,submission_id):
-    token="***REMOVED***"
+    token="REDACTED"
     base_url = 'https://www.crowdai.org/api/v1/submissions/'
     self.headers = { 'Authorization': 'Token token=' + token, "Content-Type": "application/json" }
     self.url = base_url + submission_id
@@ -12,11 +12,11 @@ class RailsAPI(object):
     response = requests.put(self.url, params=payload, headers=self.headers, verify=False)
     print response
 
-    # curl -X PUT -G http://localhost:3000/api/v1/submissions/24 -d "score=999999" -d "score_secondary=123456" -H 'Authorization: Token token="***REMOVED***"'
+    # curl -X PUT -G http://localhost:3000/api/v1/submissions/24 -d "score=999999" -d "score_secondary=123456" -H 'Authorization: Token token="REDACTED"'
 
 
 
 
 
 
-  curl -X PUT -G https://www.crowdai.org/api/v1/submissions/20004 -d "score=999999" -d "score_secondary=123456" -H 'Authorization: Token token="***REMOVED***"'
+  curl -X PUT -G https://www.crowdai.org/api/v1/submissions/20004 -d "score=999999" -d "score_secondary=123456" -H 'Authorization: Token token="REDACTED"'

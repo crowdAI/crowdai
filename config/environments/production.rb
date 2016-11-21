c106cc5199bb55763ae04000d31f065d519dedb6
Sean Carroll <sfcarroll@gmail.com>
2016-11-21 10:16:21 +0100

    fixes typo in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #config.action_mailer.default_url_options = { host: ENV["HOST"] }
-  config.actiont_mailer.default_url_options = { host: ENV["HOST"] }
+  config.action_mailer.default_url_options = { host: ENV["HOST"] }
   #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.perform_deliveries = true
   #config.action_mailer.raise_delivery_errors = false

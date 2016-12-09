97dde66cb775e4ea54f8de0c66846b8816ee9e23
Sean Carroll <sfcarroll@gmail.com>
2016-12-09 14:38:37 +0100

    email preferences

--- spec/routing/email_preferences_routing_spec.rb
+++ spec/routing/email_preferences_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe EmailPreferencesController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/email_preferences").to route_to("email_preferences#index")
+    it "does not route to #index" do
+      expect(:get => "/participants/sean/email_preferences").not_to be_routable
     end
 
-    it "routes to #new" do
-      expect(:get => "/email_preferences/new").to route_to("email_preferences#new")
+    it "does not route to #new" do
+      expect(:get => "/participants/sean/email_preferences/new").not_to be_routable
     end
 
-    it "routes to #show" do
-      expect(:get => "/email_preferences/1").to route_to("email_preferences#show", :id => "1")
+    it "does not route to #show" do
+      expect(:get => "/participants/sean/email_preferences/1").not_to be_routable
     end
 
     it "routes to #edit" do
-      expect(:get => "/email_preferences/1/edit").to route_to("email_preferences#edit", :id => "1")
+      expect(:get => "/participants/sean/email_preferences/1/edit").to route_to("email_preferences#edit", participant_id: 'sean', :id => "1")
     end
 
-    it "routes to #create" do
-      expect(:post => "/email_preferences").to route_to("email_preferences#create")
+    it "does not route to #create" do
+      expect(:post => "/participants/sean/email_preferences").not_to be_routable
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/email_preferences/1").to route_to("email_preferences#update", :id => "1")
+      expect(:put => "/participants/sean/email_preferences/1").to route_to("email_preferences#update", participant_id: 'sean', :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/email_preferences/1").to route_to("email_preferences#update", :id => "1")
+      expect(:patch => "/participants/sean/email_preferences/1").to route_to("email_preferences#update", participant_id: 'sean', :id => "1")
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/email_preferences/1").to route_to("email_preferences#destroy", :id => "1")
+    it "does not route to #destroy" do
+      expect(:delete => "/participants/sean/email_preferences/1").not_to be_routable
     end
 
   end

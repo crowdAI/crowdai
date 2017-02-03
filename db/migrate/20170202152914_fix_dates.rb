c527393743bee8137a0309e7ccadbc154e1e86d7
Sean Carroll <sfcarroll@gmail.com>
2017-02-03 14:45:01 +0100

    data conversion script

--- db/migrate/20170202152914_fix_dates.rb
+++ db/migrate/20170202152914_fix_dates.rb
@@ -1,10 +1,21 @@
 class FixDates < ActiveRecord::Migration[5.0]
   def change
-    c = Challenge.all
-    c.each do |challenge|
-      challenge.start_dttm = challenge.timeline.start_dttm
-      challenge.end_dttm = challenge.timeline.end_dttm
-      challenge.save!
-    end
+    execute "
+    UPDATE challenges
+SET start_dttm = (SELECT e.event_time
+                  FROM events e
+                  WHERE e.challenge_id = challenges.id
+                    AND e.seq = (SELECT min(f.seq)
+                                   FROM events f
+                                  WHERE f.challenge_id = challenges.id));"
+   execute "
+UPDATE challenges
+SET end_dttm = (SELECT e.event_time
+                  FROM events e
+                  WHERE e.challenge_id = challenges.id
+                    AND e.seq = (SELECT max(f.seq)
+                                   FROM events f
+                                  WHERE f.challenge_id = challenges.id));"
+
   end
 end

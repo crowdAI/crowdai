31e6a809a3ec91c82522bca885d9526cc25d6c30
Sean Carroll <sfcarroll@gmail.com>
2017-05-23 19:53:06 +0200

    markdown editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,20 @@
+// ---------------------- Gems ---------------------- //
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
 //= require cocoon
+//= require rails.validations
 
 //= require turbolinks
-//= require site
-//= require subnav_tabs
-//= require rails.validations
-//= require inline_validations
+
+// ---------------------- Modules ---------------------- //
+//= require modules/site
+//= require modules/subnav_tabs
+//= require modules/inline_validations
+//= require modules/markdown_preview
+
+// ---------------------- Pages ---------------------- //
+
 
 
 $(document).on('ajax:error', function(xhr, status, error) {
--- app/assets/javascripts/controllers/posts_controller.js
+++
@@ -1,8 +0,0 @@
-$(document).on('turbolinks:load', function() {
-  /* --------------------------------- posts / new --------------------------------------- */
-  if (!_.isEmpty(gon) && gon.rails.controller == 'posts' && gon.rails.action == 'new') {
-    expandSidebar();
-    adjustProgress(gon.percent_progress);
-    adjustText(gon.percent_progress);
-  } /* posts / new */
-});

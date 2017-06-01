394b70245f2e83d9d89794972c822cefd2bafb41
Sean Carroll <sfcarroll@gmail.com>
2017-06-01 16:04:26 +0200

    fix footer margin issues

--- app/concepts/footer/cell.rb
+++ app/concepts/footer/cell.rb
@@ -1,7 +1,16 @@
 class Footer::Cell < Cell::Concept
+
   def show
     render
   end
 
-  
+  def footer_class
+    if controller.controller_name == 'registrations' ||
+       (controller.controller_name == 'challenges' && controller.action_name == 'edit')
+          return 'no-margin-top'
+    else
+      return ''
+    end
+  end
+
 end

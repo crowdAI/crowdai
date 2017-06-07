ed6cc6343fd6c79acdc6620d994309d92d4bfcbc
Sean Carroll <sfcarroll@gmail.com>
2017-06-07 17:31:21 +0200

    counters

---
+++ app/concepts/challenge/cells/statistic.rb
@@ -0,0 +1,39 @@
+class Challenge::Cell::Statistic < Template::Cell
+  inherit_views Challenge::Cell
+
+  def show
+    render :statistic
+  end
+
+  def stat_type
+    options[:type]
+  end
+
+  def challenge
+    model
+  end
+
+  def stat_count
+    case stat_type
+    when 'submission'
+      formatted_count(challenge.submission_count)
+    when 'view'
+      formatted_count(challenge.page_views)
+    when 'participant'
+      formatted_count(challenge.participant_count)
+    end
+  end
+
+  def formatted_count(counter)
+    if counter > 1000
+      "#{sprintf('%.1f', counter/1000).chomp('.0')} k"
+    else
+      counter
+    end
+  end
+
+  def stat_text
+    "#{stat_type.titlecase}".pluralize(challenge.submission_count).html_safe
+  end
+
+end

b4775830e4e01ddabc852f43bbc712439c3b47e5
Sean Carroll <sfcarroll@gmail.com>
2017-06-07 19:45:14 +0200

    staging CDN

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local       = false
@@ -12,27 +11,13 @@ Rails.application.configure do
   config.force_ssl = true
   config.log_level = :debug
   config.i18n.fallbacks = true
-
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-
-  #config.action_mailer.default_url_options = { host: ENV["HOST"] }
   config.action_mailer.default_url_options = { host: ENV["HOST"] }
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.perform_deliveries = true
-  #config.action_mailer.raise_delivery_errors = false
-  #config.action_mailer.default :charset => "utf-8"
-
-  #config.action_mailer.smtp_settings = {
-  #  :address   => ENV["SMTP_ADDRESS"],
-  #  :port      => ENV["SMTP_PORT"],
-  #  :enable_starttls_auto => true, # detects and uses STARTTLS
-  #  :user_name => ENV["SMTP_USERNAME"],
-  #  :password  => ENV["SMTP_PASSWORD"],
-  #  :authentication => 'login', # Mandrill supports 'plain' or 'login'
-  #  :domain => ENV["SMTP_DOMAIN"]
-  #}
-
+  config.action_controller.asset_host = "https://#{ENV['CLOUDFRONT_ASSETS_DOMAIN']}"
 end
 Rails.application.routes.default_url_options[:host] = ENV['HOST']
+CarrierWave.configure do |config|
+  config.asset_host = "https://#{ENV['CLOUDFRONT_IMAGES_DOMAIN']}"
+end

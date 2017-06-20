1116c15be3fc1997549adfdd219ad7b40d4123ab
Sean Carroll <sfcarroll@gmail.com>
2017-06-20 12:44:55 +0200

    auto fill urls

--- app/assets/javascripts/pages/participants_edit.js
+++ app/assets/javascripts/pages/participants_edit.js
@@ -1,1 +1,10 @@
-$('.url')
+$(document).on('turbolinks:load', function() {
+  $('.url').blur(function() {
+    var self = this;
+    $(self).val($(self).val().toLowerCase());
+    var url = $(self).val();
+    if ((url.substr(0,7) !== 'http://') || (url.substr(0,8) !== 'https://')){
+      $(self).val('https://' + url);
+    }
+  });
+});

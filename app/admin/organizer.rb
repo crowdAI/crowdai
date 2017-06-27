5058123cdde3eb57d7e7cb11653eb946e9651cde
Sean Carroll <sfcarroll@gmail.com>
2017-06-27 17:43:48 +0200

    admin - organizer

--- app/admin/organizer.rb
+++ app/admin/organizer.rb
@@ -10,6 +10,21 @@ ActiveAdmin.register Organizer do
     selectable_column
     column :id
     column :organizer
+    column :approved
     actions
   end
+
+  permit_params :organizer, :address, :description, :approved, :image_file, :tagline
+  form do |f|
+    f.inputs "Challenge" do
+      f.input :organizer
+      f.input :address
+      f.input :description
+      f.input :approved, as: :boolean
+      f.input :image_file
+      f.input :tagline
+    end
+    f.actions
+  end
+
 end

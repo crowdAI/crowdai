b574fbbd70e0ae56b40e8687365e210edbb969e6
Sean Carroll <sfcarroll@gmail.com>
2017-08-11 11:39:27 +0200

    redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
 require 'sidekiq'
+require 'sidekiq-scheduler'
 
 Sidekiq.configure_client do |config|
   config.redis = { size: 1 }
@@ -8,3 +9,11 @@ Sidekiq.configure_server do |config|
   config.logger.level = Logger::DEBUG
   config.redis = { size: 4 }
 end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDISTOGO_URL"] }
+end unless ENV['REDISTOGO_URL'].blank?
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDISTOGO_URL"] }
+end unless ENV['REDISTOGO_URL'].blank?

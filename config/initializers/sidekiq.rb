e78ee27c8691631b7f6b2fa6abc14e1fccbb5b11
Sean Carroll <sfcarroll@gmail.com>
2017-08-11 14:23:03 +0200

    logs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,27 +10,5 @@ if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
 
-module Sidekiq
-  module Middleware
-    module Server
-      class TaggedLogger
-
-        def call(worker, item, queue)
-          tag = "#{worker.class.to_s} #{SecureRandom.hex(12)}"
-          ::Rails.logger.tagged(tag) do
-            job_info = "Start at #{Time.now.to_default_s}: #{item.inspect}"
-            ::Rails.logger.info(job_info)
-            yield
-          end
-        end
-
-      end
-    end
-  end
-end
-
-Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Middleware::Server::TaggedLogger
-  end
-end
+Sidekiq::Logging.logger.level = Logger::DEBUG
+Rails.logger = Sidekiq::Logging.logger

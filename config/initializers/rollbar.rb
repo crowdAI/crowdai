5ee1ccb4afa83e4d1cbfffda428ae7e5a23b361b
Sean Carroll <sfcarroll@gmail.com>
2017-08-14 11:23:52 +0200

    disable rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -5,9 +5,9 @@ Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
   config.enabled = false
-  unless Rails.env.production?
-    config.enabled = false
-  end
+  #unless Rails.env.production?
+  #  config.enabled = false
+  #end
 
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,

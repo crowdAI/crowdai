643e4872e8f0f7f27f8f8525bbc37d847ea7dd9f
Sean Carroll <sfcarroll@gmail.com>
2017-08-25 20:42:56 +0200

    video playback

--- app/assets/javascripts/pages/leaderboards_index.js
+++ app/assets/javascripts/pages/leaderboards_index.js
@@ -19,7 +19,7 @@ $(document).on('turbolinks:load', function() {
         clearTimeout(scrollTimeout);
       }
       scrollTimeout = setTimeout(function() {
-        //startVideoPlayers();
+        startVideoPlayers();
       }, 2000);
     });
   }

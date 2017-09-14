b566b958c27f0476cf84a5be7d19d41b89c847c0
Sean Carroll <sfcarroll@gmail.com>
2017-09-14 16:13:36 +0200

    email preferences

--- app/controllers/email_preferences_controller.rb
+++ app/controllers/email_preferences_controller.rb
@@ -2,14 +2,12 @@ class EmailPreferencesController < ApplicationController
   before_action :set_participant
   before_action :set_edit_token, only: [:edit]
   before_action :set_update_token, only: [:update]
-  before_action :unsubscribe_token_or_authenticate
+  before_action :email_preferences_token_or_authenticate
 
   def edit
-    #authorize @email_preference
   end
 
   def update
-    #authorize @email_preference
     if @email_preference.update(email_preference_params)
       if @token
         redirect_to edit_participant_email_preference_path(@participant, @email_preference, unsubscribe_token: @token),
@@ -23,60 +21,59 @@ class EmailPreferencesController < ApplicationController
     end
   end
 
-
   private
-    def set_email_preference
-      authenticate_participant!
-      @email_preference = EmailPreference.find(params[:id])
-    end
+  def set_email_preference
+    authenticate_participant!
+    @email_preference = EmailPreference.find(params[:id])
+  end
 
-    def set_participant
-      @participant = Participant.friendly.find(params[:participant_id])
-    end
+  def set_participant
+    @participant = Participant.friendly.find(params[:participant_id])
+  end
 
-    def email_preference_params
-      params.require(:email_preference)
-            .permit(:participant_id,
-                    :newsletter,
-                    :challenges_followed,
-                    :mentions,
-                    :receive_every_email,
-                    :receive_daily_digest,
-                    :receive_weekly_digest,
-                    :unsubscribe_token)
-    end
+  def email_preference_params
+    params.require(:email_preference)
+          .permit(:participant_id,
+                  :newsletter,
+                  :challenges_followed,
+                  :mentions,
+                  :receive_every_email,
+                  :receive_daily_digest,
+                  :receive_weekly_digest,
+                  :unsubscribe_token)
+  end
 
-    def set_edit_token
-      @token = params[:unsubscribe_token] if params[:unsubscribe_token]
-    end
+  def set_edit_token
+    @token = params[:unsubscribe_token] if params[:unsubscribe_token]
+  end
 
-    def set_update_token
-      if params[:email_preference][:unsubscribe_token]
-        @token = params[:email_preference][:unsubscribe_token]
-        params[:email_preference].delete(:unsubscribe_token)
+  def set_update_token
+    if params[:email_preference][:unsubscribe_token]
+      @token = params[:email_preference][:unsubscribe_token]
+      params[:email_preference].delete(:unsubscribe_token)
+    end
+  end
+
+  def email_preferences_token_or_authenticate
+    if @token.present?
+      status = EmailPreferencesTokenService.new(current_participant).validate_token(@token)
+      case status
+      when 'invalid_participant'
+        flash[:error] = "The email preferences link is not valid for the currently logged in participant."
+        redirect_to '/'
+      when 'valid_token'
+        authenticate_participant!
+        @email_preference = current_participant.email_preferences.first
+      when 'token_expired'
+        flash[:error] = 'The email preferences link has expired.'
+        redirect_to new_participant_session_path
+      when 'invalid_token'
+        flash[:error] = "The email preferences link is invalid."
+        redirect_to new_participant_session_path
       end
+    else
+      set_email_preference
     end
-
-
-    def unsubscribe_token_or_authenticate
-      if @token.present?
-        email = Email.where(participant_id: @participant.id, email_preferences_token: @token)[0]
-        if current_participant && (email.participant_id != current_participant.id)
-          flash[:error] = "The unsubscribe link is not valid for the currently logged in participant."
-          redirect_to '/'
-        end
-        if email.blank?
-          flash[:error] = "The unsubscribe link is invalid."
-          redirect_to new_participant_session_path
-        end
-        if email.present? && email.token_expiration_dttm < DateTime.current
-          flash[:error] = 'The unsubscribe link has expired.'
-          redirect_to new_participant_session_path
-        end
-        @email_preference = @participant.email_preferences.first
-      else  # no token in url - user has not come via an email link
-        set_email_preference
-      end
-    end
+  end
 
 end

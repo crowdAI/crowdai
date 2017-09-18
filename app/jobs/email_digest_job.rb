aff25516ed7a773cb59495cac363918915a6b117
Sean Carroll <sfcarroll@gmail.com>
2017-09-18 10:50:01 +0200

    fixes digest issue

--- app/jobs/email_digest_job.rb
+++ app/jobs/email_digest_job.rb
@@ -2,26 +2,27 @@ class EmailDigestJob < ApplicationJob
   queue_as :digest
 
   def perform(digest_params)
-    digest_type = digest_params[:digest_type]
-    Rails.logger.info("[CROWDAI] digest_params #{digest_params}")
+    digest_type = digest_params['digest_type']
+    Rails.logger.info("[EmailDigestJob] digest_params #{digest_params}")
 
     if digest_type == 'daily'
-      Rails.logger.info("[CROWDAI] processing DAILY")
+      Rails.logger.info("[EmailDigestJob] processing DAILY")
       daily_digest_participant_ids.each do |participant_id|
-        Rails.logger.info("[CROWDAI] processing #{participant_id}")
+        Rails.logger.info("[EmailDigestJob] processing #{participant_id}")
         EmailDigestMailer.new.sendmail(participant_id,'daily')
       end
     end
     if digest_type == 'weekly'
+      Rails.logger.info("[EmailDigestJob] processing WEEKLY")
       weekly_digest_participant_ids.each do |participant_id|
+        Rails.logger.info("[EmailDigestJob] processing #{participant_id}")
         EmailDigestMailer.new.sendmail(participant_id,'weekly')
       end
     end
   end
 
   def daily_digest_participant_ids
-    return [1]
-    #EmailPreference.where(receive_daily_digest: true).ids
+    EmailPreference.where(receive_daily_digest: true).ids
   end
 
   def weekly_digest_participant_ids

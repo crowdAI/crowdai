c95ee5b49394275b230e612cf465d0490a2b0a95
Sean Carroll <sfcarroll@gmail.com>
2017-10-09 15:41:05 +0200

    faq

--- spec/routing/pages_routing_spec.rb
+++ spec/routing/pages_routing_spec.rb
@@ -4,15 +4,19 @@ RSpec.describe PagesController, type: :routing do
   describe "routing" do
 
     it "routes to #contact" do
-      expect(:get => "/pages/contact").to route_to("pages#contact")
+      expect(:get => "/contact").to route_to("pages#contact")
     end
 
     it "routes to #new" do
-      expect(:get => "/pages/privacy").to route_to("pages#privacy")
+      expect(:get => "/privacy").to route_to("pages#privacy")
     end
 
     it "routes to #show" do
-      expect(:get => "/pages/terms").to route_to("pages#terms")
+      expect(:get => "/terms").to route_to("pages#terms")
+    end
+
+    it "routes to #faq" do
+      expect(:get => "/faq").to route_to("pages#faq")
     end
 
   end

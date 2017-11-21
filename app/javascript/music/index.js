7f2b8d28f1d189e501ee67338ebf0f2d63eee6c1
Sean Carroll <sfcarroll@gmail.com>
2017-11-21 14:39:41 +0100

    wip music

---
+++ app/javascript/music/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Music from './components/Music';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();

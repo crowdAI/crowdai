05fe42b4fca5b77457b6bfb195ce2efb4bccca81
Sean Carroll <sfcarroll@gmail.com>
2017-11-23 16:10:03 +0100

    music challenge

--- app/javascript/music/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Music from './components/Music';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();

783fe1af1ec67a4cd7c815b3b174bd0416d4113a
Sean Carroll <sfcarroll@gmail.com>
2017-11-23 09:25:01 +0100

    active admin config

--- app/admin/organizer_applications.rb
+++ app/admin/organizer_applications.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register OrganizerApplication do
-  
+
+  controller do
+    def permitted_params
+      params.permit!
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :contact_name
+    column :email
+    column :phone
+    column :organization
+    actions
+  end
+
+
+
 end

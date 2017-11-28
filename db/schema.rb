9c8819216721a9fbcc3a8698e1f2e71c895207c9
Sean Carroll <sfcarroll@gmail.com>
2017-11-28 10:54:25 +0100

    challenge round data cleanse

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128092442) do
+ActiveRecord::Schema.define(version: 20171128095100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

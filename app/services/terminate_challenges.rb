2a4799d3d780605aed6769893087eeeedecac25d
Sean Carroll <sfcarroll@gmail.com>
2017-11-30 15:48:02 +0100

    clef

--- app/services/terminate_challenges.rb
+++ app/services/terminate_challenges.rb
@@ -1,12 +1,11 @@
 class TerminateChallenges
 
   def call
-    expiring_challenges = Challenge
-                   .where(status_cd: :running)
-                   .where.not(end_dttm: nil)
-                   .where("end_dttm <= current_timestamp")
-    expiring_challenges.each do |c|
-      c.update(status: :completed)
+    running_challenges = Challenge.where(status_cd: :running)
+    running_challenges.each do |c|
+      if c.end_dttm <= Time.current
+        c.update(status: :completed)
+      end
     end
   end
 

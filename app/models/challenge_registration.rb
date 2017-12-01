400021921d57ca768608f14eaa3324a30d64f094
Sean Carroll <sfcarroll@gmail.com>
2017-12-01 12:46:37 +0100

    clef challenge counts

--- app/models/challenge_registration.rb
+++
@@ -1,7 +0,0 @@
-class ChallengeRegistration < ApplicationRecord
-  belongs_to :challenge
-  belongs_to :participant
-  mount_uploader :eua_file, EuaUploader
-
-  as_enum :status, [:requested, :submitted, :registered, :rejected], map: :string
-end

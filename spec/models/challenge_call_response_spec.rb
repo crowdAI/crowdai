46c99435383b8bf15dccb40fcd3d34bf7dff5298
Sean Carroll <sfcarroll@gmail.com>
2017-12-07 16:34:16 +0100

    challenge_calls

--- spec/models/challenge_call_response_spec.rb
+++ spec/models/challenge_call_response_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ChallengeCallResponse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { is_expected.to belong_to :challenge_call }
+  end
 end

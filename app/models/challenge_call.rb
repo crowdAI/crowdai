864663f7d56f253a4018a5719081443c42aa3a87
Sean Carroll <sfcarroll@gmail.com>
2017-12-22 10:10:51 +0100

    markdown on challenge call pages

--- app/models/challenge_call.rb
+++ app/models/challenge_call.rb
@@ -3,12 +3,20 @@ class ChallengeCall < ApplicationRecord
   friendly_id :title, use: [:slugged, :finders, :history]
   has_many :challenge_call_responses
   belongs_to :organizer
+  before_save :cache_rendered_markdown
 
   def should_generate_new_friendly_id?
     title_changed?
   end
 
   def call_closed?
-    self.closing_date.present? && self.closing_date < Time.now.utc 
+    self.closing_date.present? && self.closing_date < Time.now.utc
   end
+
+  def cache_rendered_markdown
+    if description_markdown_changed?
+      self.description = Kramdown::Document.new(self.description_markdown,{coderay_line_numbers: nil}).to_html
+    end
+  end
+
 end

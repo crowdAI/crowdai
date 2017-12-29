1f31c3b528e014ccd65877704ba32a4d260299b6
Sean Carroll <sfcarroll@gmail.com>
2017-12-29 13:41:18 +0100

    cleanup jbuilder

--- spec/services/notification_service_spec.rb
+++ spec/services/notification_service_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe NotificationService do
   let!(:participant) { create :participant }
 
   context 'comment' do
-    let(:comment) { create :comment }
-    
+    let!(:comment) { create :comment }
+    it 'creates a notification' do
+      expect {
+        described_class.new(participant.id,comment,'comment').call
+      }.to change(Notification, :count).by(1)
+    end
   end
 
 end

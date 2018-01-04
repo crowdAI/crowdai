855e768a257edf77aefb5fae458c2067f64e90e7
Sean Carroll <sfcarroll@gmail.com>
2018-01-04 22:26:19 +0100

    at mentions jquery

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
+  include Mentions
   has_paper_trail
   belongs_to :topic
   belongs_to :participant, optional: true
   has_many :votes, as: :votable
   has_many :notifications, as: :notifiable
-
-  before_validation :cache_rendered_markdown
+  before_save :cache_rendered_markdown
 
   #default_scope { order('created_at desc') }
 
@@ -16,10 +16,26 @@ class Comment < ApplicationRecord
     super || NullParticipant.new
   end
 
-  private
+
   def cache_rendered_markdown
     if comment_markdown_changed?
-      self.comment = Kramdown::Document.new(self.comment_markdown,{coderay_line_numbers: nil}).to_html
+      new_mentions = mentions('comment_markdown') - mentions('comment_markdown_before_last_save')
+      rendered_html = Kramdown::Document.new(self.comment_markdown,{coderay_line_numbers: nil}).to_html
+      self.comment = mentions_to_links(comment_text: rendered_html, mentions: new_mentions)
+      #MentionsNotificationsJob.perform_later(mentioned_names: new_mentions, comment_id: self.id)
     end
   end
+
+  def mentioned_participants(mentioned_names:)
+    Participant.where(name: mentioned_names).pluck(:name).join('|')
+  end
+
+  def mentions_to_links(comment_text:,mentions:)
+    comment_text.gsub! /@(#{mentions})/ do |match|
+      participant = Participant.find_by(name: match)
+      "<a href='/participants/#{participant.slug}'>@#{$1}</a>"
+    end
+    return comment_text
+  end
+
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -22,4 +22,67 @@ describe Comment do
   context 'validations' do
     it { is_expected.to validate_presence_of(:vote_count) }
   end
+
+  context 'mentions' do
+    describe 'single mention' do
+      let!(:comment) { create :comment }
+      before do
+        comment.comment_markdown = '@sean'
+      end
+      it { expect(comment.mentions('comment_markdown')).to eq(['sean'])}
+    end
+
+    describe 'multiple mentions' do
+      let!(:comment) { create :comment }
+      before do
+        comment.comment_markdown = '@sean blah blah @francis'
+      end
+      it { expect(comment.mentions('comment_markdown')).to eq(['sean','francis'])}
+    end
+
+    describe 'mentioned_participants' do
+      context 'single participant' do
+        let(:comment) { create :comment }
+        let(:participant) { create :participant }
+        it { expect(comment.mentioned_participants(mentioned_names: participant.name)).to eq(participant.name) }
+      end
+      context 'multiple participant' do
+        let(:comment) { create :comment }
+        let(:participant1) { create :participant }
+        let(:participant2) { create :participant }
+        it { expect(comment.mentioned_participants(mentioned_names: [participant1.name,participant2.name] )).to eq("#{participant1.name}|#{participant2.name}") }
+      end
+      context 'missing name' do
+        let(:comment) { create :comment }
+        let(:participant) { create :participant }
+        it { expect(comment.mentioned_participants(mentioned_names: [participant.name,'wrong'] )).to eq("#{participant.name}") }
+      end
+    end
+
+
+    describe 'mentions_to_links' do
+      let!(:mentions) { ['Sean','joe']}
+      let!(:comment) { create :comment, comment_markdown: '@sean blah blah @joe asda' }
+      it do
+        mentioned_participants = double("mentioned_participants", message: 'Sean|joe|kim')
+        expect(comment.mentions_to_links(comment_text: comment_text, mentions: mentions)).to eq('a2333')
+      end
+    end
+
+  end
+
+=begin
+    describe 'do not re-mention if not changed' do
+      let!(:comment) { create :comment, comment_markdown: '@sean blah blah @francis' }
+      before do
+        comment.save
+        comment.reload
+        comment.comment_markdown = '@sean blah blah, @francis and @joe as well'
+      end
+      it { expect(comment.mentions('comment_markdown')).to eq(['joe'])}
+    end
+  end
+=end
+
+
 end

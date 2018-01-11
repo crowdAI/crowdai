7dd4d703c83443feea57264a476de1373e958b9f
Sean Carroll <sfcarroll@gmail.com>
2018-01-11 09:59:21 +0100

    rpsec refactoring

--- app/services/markdown_service.rb
+++ app/services/markdown_service.rb
@@ -1,29 +1,22 @@
 class MarkdownService
 
-  def initialize(markdown:, markdown_old: nil, mentions_cache: nil)
+  def initialize(markdown:, mentions_cache: nil)
     @markdown = markdown
     @mentions_cache = JSON.parse(mentions_cache) if mentions_cache.present?
   end
 
-
   def call
-
-    return [rendered_html,mentioned_participant_ids]
-  end
-
-  def cache_rendered_markdown
-    if comment_markdown_changed?
-      rendered_html = Kramdown::Document.new(self.comment_markdown,{coderay_line_numbers: nil}).to_html
-      new_mentions = JSON.parse(self.mentions_cache)
-      self.mentions_cache = nil
-      self.comment, mentioned_participant_ids = mentions_to_links(rendered_html: rendered_html, mentions: new_mentions)
-      MentionsNotificationsJob.perform_later(mentioned_participant_ids: mentioned_participant_ids, comment_id: self.id)
+    return [nil, nil] if @markdown.nil?
+    rendered_html = Kramdown::Document.new(@markdown,{coderay_line_numbers: nil}).to_html
+    if @mentions_cache.present?
+      rendered_html, mention_participant_ids = mentions_to_links(rendered_html: rendered_html, mentions_cache: @mentions_cache)
     end
+    return [rendered_html, mention_participant_ids]
   end
 
-  def mentions_to_links(rendered_html:,mentions:)
+  def mentions_to_links(rendered_html:,mentions_cache:)
     mention_participant_ids = []
-    mentions.each do |mention|
+    mentions_cache.each do |mention|
       rendered_html.gsub! /@(#{mention['name']})/ do |match|
         mention_participant_ids << mention['id']
         "<a href='/participants/#{mention['id']}' target='_blank'>@#{$1}</a>"
---
+++ spec/services/markdown_service_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe MarkdownService do
+
+  describe 'simple markdown string' do
+    let(:markdown) { '### Some markdown' }
+    it { expect(MarkdownService.new(markdown: markdown).call).to eq(["<h3 id=\"some-markdown\">Some markdown</h3>\n",nil]) }
+  end
+
+  describe '#mentions_to_links' do
+    let(:markdown) { '#ignore'}
+    let(:rendered_html) { "<h3 id=\"some-markdownn-sean-some-text\">Some markdown\\n @Sean some text</h3>\n" }
+    let(:mentions_cache) { JSON.parse("[{\"id\":1,\"name\":\"Sean\"},{\"id\":22,\"name\":\"Hanno\"}]") }
+    it { expect(MarkdownService.new(markdown: markdown).mentions_to_links(rendered_html: rendered_html, mentions_cache: mentions_cache))
+           .to eq(["<h3 id=\"some-markdownn-sean-some-text\">Some markdown\\n <a href='/participants/1' target='_blank'>@Sean</a> some text</h3>\n", [1]]) }
+  end
+
+  describe 'single mentions' do
+    let(:markdown) { '### Some markdown\n @Sean some text' }
+    let(:mentions_cache) { "[{\"id\":1,\"name\":\"Sean\"}]" }
+    it { expect(MarkdownService.new(markdown: markdown, mentions_cache: mentions_cache).call)
+           .to eq(["<h3 id=\"some-markdownn-sean-some-text\">Some markdown\\n <a href='/participants/1' target='_blank'>@Sean</a> some text</h3>\n", [1]]) }
+  end
+
+  describe 'multiple mentions' do
+    let(:markdown) { '### Some markdown\n @Sean some text @Hanno' }
+    let(:mentions_cache) { "[{\"id\":1,\"name\":\"Sean\"},{\"id\":22,\"name\":\"Hanno\"}]" }
+    it { expect(MarkdownService.new(markdown: markdown, mentions_cache: mentions_cache).call)
+           .to eq(["<h3 id=\"some-markdownn-sean-some-text-hanno\">Some markdown\\n <a href='/participants/1' target='_blank'>@Sean</a> some text <a href='/participants/22' target='_blank'>@Hanno</a></h3>\n", [1, 22]]) }
+  end
+
+  describe 'mention in cache but removed from markdown' do
+    let(:markdown) { '### Some markdown\n @Sean some text' }
+    let(:mentions_cache) { "[{\"id\":1,\"name\":\"Sean\"},{\"id\":22,\"name\":\"Hanno\"}]" }
+    it { expect(MarkdownService.new(markdown: markdown, mentions_cache: mentions_cache).call)
+           .to eq(["<h3 id=\"some-markdownn-sean-some-text\">Some markdown\\n <a href='/participants/1' target='_blank'>@Sean</a> some text</h3>\n", [1]]) }
+  end
+
+  describe 'markdown is nil' do
+    let(:markdown) { nil }
+    it { expect(MarkdownService.new(markdown: markdown).call).to eq([nil,nil]) }
+  end
+
+end

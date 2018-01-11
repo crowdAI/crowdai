ee6e5fc5b9977ef77acc3ca11547d0ca1e88bf9b
Sean Carroll <sfcarroll@gmail.com>
2018-01-11 13:58:30 +0100

    mentions job

--- app/jobs/mentions_notifications_job.rb
+++ app/jobs/mentions_notifications_job.rb
@@ -1,10 +1,10 @@
 class MentionsNotificationsJob < ApplicationJob
-  queue_as :default
+  queue_as :notifications
 
   def perform(mentioned_participant_ids:,comment_id:)
     comment = Comment.find(comment_id)
-    mentioned_participant_ids.each do participant_id
-      NotificationService.new(participant.id,comment,'mention')
+    mentioned_participant_ids.each do |participant_id|
+      NotificationService.new(participant_id,comment,'mention')
     end
   end
 
--- spec/jobs/mentions_notifications_job_spec.rb
+++ spec/jobs/mentions_notifications_job_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe MentionsNotificationsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  let!(:participant) { create :participant }
+  let!(:comment) { create :comment }
+
+  describe 'queues the job' do
+
+    subject(:job) { described_class.perform_later(mentioned_participant_ids: [participant.id], comment_id: comment.id) }
+
+    it 'queues the job' do
+      expect { job }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+    end
+
+    it 'is placed on the notifications queue' do
+      expect(described_class.new.queue_name).to eq('notifications')
+    end
+
+    it 'executes with no errors' do
+      perform_enqueued_jobs { job }
+    end
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
 end

22031bb438f07c9d0ffe45e385bee5600c738c76
Sean Carroll <sfcarroll@gmail.com>
2018-02-27 10:14:29 +0100

    meta in view

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe SubmissionsController, type: :controller do
         challenge: challenge,
         participant: participant }
   end
+  let!(:meta) { create :submission, :with_meta, challenge: challenge, participant: participant }
   let(:challenge) { create :challenge }
   let(:participant) { create :participant }
 
@@ -17,15 +18,22 @@ RSpec.describe SubmissionsController, type: :controller do
   end
 
   describe 'GET #index' do
-    before { get :index, params: { challenge_id: submission_1.challenge_id } }
-    it { expect(assigns(:submissions).sort).to eq [submission_1, submission_2, submission_3].sort }
+    before { get :index, params: { challenge_id: challenge.id } }
+    it { expect(assigns(:submissions).sort).to eq [submission_1, submission_2, submission_3, meta].sort }
     it { expect(response).to render_template :index }
   end
 
   describe 'GET #show' do
-    before { get :show, params: { challenge_id: submission_1.challenge_id, id: submission_1.id } }
-    it { expect(assigns(:submission)).to eq submission_1 }
-    it { expect(response).to render_template :show }
+    context do
+      before { get :show, params: { challenge_id: challenge.id, id: submission_1.id } }
+      it { expect(assigns(:submission)).to eq submission_1 }
+      it { expect(response).to render_template :show }
+    end
+    context 'renders with meta' do
+      before { get :show, params: { challenge_id: meta.challenge_id, id: meta.id } }
+      it { expect(assigns(:submission)).to eq meta }
+      it { expect(response).to render_template :show }
+    end
   end
 
 end

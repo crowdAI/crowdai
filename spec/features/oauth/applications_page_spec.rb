d8286b381b1aff7f9693e539a86fb4449beaf040
Sean Carroll <sfcarroll@gmail.com>
2018-02-27 16:17:09 +0100

    secure access to OAuth for admins only

---
+++ spec/features/oauth/applications_page_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature "Accessing the OAuth provider page" do
+  let!(:participant) { create :participant }
+  let!(:admin) { create :participant, :admin }
+
+  scenario 'public user' do
+    visit "/oauth/applications"
+    expect(page).to have_content 'Log in'
+    expect(page).not_to have_content 'OAuth2'
+  end
+
+  scenario 'participant user' do
+    log_in(participant)
+    visit "/oauth/applications"
+    expect(page).to have_content 'You are already signed in'
+    expect(page).not_to have_content 'OAuth2'
+  end
+
+  scenario 'admin user' do
+    log_in(admin)
+    visit "/oauth/applications"
+    expect(page).to have_content 'OAuth2'
+  end
+end

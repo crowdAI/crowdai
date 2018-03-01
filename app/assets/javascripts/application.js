c14cc16cd5baa3fc111259d23bfb10946ee28076
Sean Carroll <sfcarroll@gmail.com>
2018-03-01 08:58:12 +0100

    replace default loader

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,10 +64,22 @@ $(document).on('turbolinks:load', function() {
   loadMathJax();
 });
 
+// Remove default Turbolinks loader
+Turbolinks.ProgressBar.prototype.refresh = function() {}
+Turbolinks.ProgressBar.defaultCSS = ""
+var loaderTimer;
+
 $(document).on('turbolinks:click', function() {
-  console.log('spinner starts');
+  loaderTimer = setTimeout(function(){
+    console.log('spinner starts');
+    $('#page-content').hide();
+    $('#loader-container').show();
+  }, 500);
 });
 
 $(document).on('turbolinks:load', function() {
+  clearTimeout(loaderTimer);
   console.log('spinner stops');
+  $('#page-content').show();
+  $('#loader-container').hide();
 });

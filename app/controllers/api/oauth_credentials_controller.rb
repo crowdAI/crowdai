22786f37a5cc76777d92418e7b423a4b79a28b86
Sean Carroll <sfcarroll@gmail.com>
2018-03-19 13:16:28 +0100

    reconfigure Doorkeeper

--- app/controllers/api/oauth_credentials_controller.rb
+++ app/controllers/api/oauth_credentials_controller.rb
@@ -1,10 +1,15 @@
 class Api::OauthCredentialsController < ApplicationController
   before_action :doorkeeper_authorize!
   before_action -> { doorkeeper_authorize! :public }, only: :show
+  before_action -> { doorkeeper_authorize! :write }, only: :update
+
   respond_to :json
 
   def show
     render json: current_resource_owner.as_json
+  end
+
+  def update
   end
 
   private

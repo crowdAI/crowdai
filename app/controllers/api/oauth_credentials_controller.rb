1ec00a27d04d989c2a59e56112d2c7e21a56a131
Sean Carroll <sfcarroll@gmail.com>
2018-03-19 11:02:45 +0100

    track master for Doorkeeper and update accordingly

--- app/controllers/api/oauth_credentials_controller.rb
+++ app/controllers/api/oauth_credentials_controller.rb
@@ -1,15 +1,16 @@
 class Api::OauthCredentialsController < ApplicationController
   before_action :doorkeeper_authorize!
+  before_action -> { doorkeeper_authorize! :public }, only: :show
   respond_to :json
 
-  def me
-    render json: current_resource_owner
+  def show
+    render json: current_resource_owner.as_json
   end
 
   private
   def current_resource_owner
     if doorkeeper_token
-      Participant.find(doorkeeper_token.resource_owner_id)
+      Participant.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
     end
   end
 end

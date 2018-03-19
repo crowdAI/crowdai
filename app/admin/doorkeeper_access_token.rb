af7c6cc6f5aa3d79d0d832a32423c4ff999b9c2c
Sean Carroll <sfcarroll@gmail.com>
2018-03-19 14:54:00 +0100

    add doorkeeper tables to admin

---
+++ app/admin/doorkeeper_access_token.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Doorkeeper::AccessToken do
+  actions :index, :show
+
+  #belongs_to :application
+  #navigation_menu :application
+
+  index do
+    column :id
+    column :resource_owner_id
+    column :application_id
+    column :expires_in
+    column :scopes
+    actions
+  end
+
+end
---
+++ app/admin/doorkeeper_application.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Doorkeeper::Application do
+  actions :index, :show
+
+=begin
+  sidebar "Oauth Access", only: [:show, :edit] do
+    ul do
+      li link_to "Access Grants", admin_doorkeeper_applications_doorkeeper_access_grant(doorkeeper_application)
+      li link_to "Access Tokens", admin_doorkeeper_applications_doorkeeper_access_grant(doorkeeper_application)
+    end
+  end
+=end
+
+  index do
+    column :id
+    column :name
+    column :redirect_uri
+    column :scopes
+    column :reponses do |res|
+      res.access_grants.count
+    end
+    column :reponses do |res|
+      res.access_tokens.count
+    end
+    actions
+  end
+
+
+end

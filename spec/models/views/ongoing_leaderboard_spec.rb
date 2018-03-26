0ea065b7f8766dc79ca8d2d50263d9a1dccb6dd2
Sean Carroll <sfcarroll@gmail.com>
2018-03-26 15:12:52 +0200

    leaderboard test coverage

--- spec/models/views/ongoing_leaderboard_spec.rb
+++ spec/models/views/ongoing_leaderboard_spec.rb
@@ -62,7 +62,7 @@ describe CurrentOngoingLeaderboard do
       grading_status: :graded,
       post_challenge: true,
       score: 40.00,
-      score_secondary: 0.003 }
+      score_secondary: 0.005 }
 
     # Submission | Participant | Score | Secondary | Post?
     # -----------|-------------|-------|------------------
@@ -84,54 +84,55 @@ describe CurrentOngoingLeaderboard do
           primary_sort_order: :ascending,
           secondary_sort_order: :ascending)
       end
-      it { expect(CurrentLeaderboard.count)
+      it { expect(OngoingLeaderboard.count)
             .to eq(2) }
-      it { expect(CurrentLeaderboard.first.participant_id)
+      it { expect(OngoingLeaderboard.first.participant_id)
             .to eq(participant1.id) }
-      it { expect(CurrentLeaderboard.first.row_num)
+      it { expect(OngoingLeaderboard.first.row_num)
             .to eq(1) }
-      it { expect(CurrentLeaderboard.first.score)
+      it { expect(OngoingLeaderboard.first.score)
             .to eq(30.05) }
-      it { expect(CurrentLeaderboard.first.score_secondary)
+      it { expect(OngoingLeaderboard.first.score_secondary)
             .to eq(0.001) }
 
-      it { expect(CurrentLeaderboard.second.participant_id)
+      it { expect(OngoingLeaderboard.second.participant_id)
             .to eq(participant2.id) }
-      it { expect(CurrentLeaderboard.second.row_num)
+      it { expect(OngoingLeaderboard.second.row_num)
             .to eq(2) }
-      it { expect(CurrentLeaderboard.second.score)
+      it { expect(OngoingLeaderboard.second.score)
             .to eq(31.00) }
-      it { expect(CurrentLeaderboard.second.score_secondary)
+      it { expect(OngoingLeaderboard.second.score_secondary)
             .to eq(0.003) }
     end
 
     describe 'order by primary desc, secondary desc' do
       # Row | Submission | Participant | Primary | Secondary
       # ----|------------|-------------|---------|----------
-      #  1  |  p2s2      |  p2         |  36.00  |  0.003
+      #  1  |  p2s3      |  p2         |  40.00  |  0.005
       #  2  |  p1s4      |  p1         |  32.05  |  0.002
       before do
         challenge.update(
           primary_sort_order: :descending,
           secondary_sort_order: :descending)
       end
-      it { expect(CurrentLeaderboard.count)
+      it { expect(OngoingLeaderboard.count)
             .to eq(2) }
-      it { expect(CurrentLeaderboard.first.participant_id)
+      it { expect(OngoingLeaderboard.first.participant_id)
             .to eq(participant2.id) }
-      it { expect(CurrentLeaderboard.first.row_num)
+      it { expect(OngoingLeaderboard.first.row_num)
             .to eq(1) }
-      it { expect(CurrentLeaderboard.first.score)
-            .to eq(36.00) }
-      it { expect(CurrentLeaderboard.first.score_secondary)
-            .to eq(0.003) }
-      it { expect(CurrentLeaderboard.second.participant_id)
+      it { expect(OngoingLeaderboard.first.score)
+            .to eq(40.00) }
+      it { expect(OngoingLeaderboard.first.score_secondary)
+            .to eq(0.005) }
+      it { expect(OngoingLeaderboard.second.participant_id)
             .to eq(participant1.id) }
-      it { expect(CurrentLeaderboard.second.row_num)
+
+      it { expect(OngoingLeaderboard.second.row_num)
             .to eq(2) }
-      it { expect(CurrentLeaderboard.second.score)
+      it { expect(OngoingLeaderboard.second.score)
             .to eq(32.05) }
-      it { expect(CurrentLeaderboard.second.score_secondary)
+      it { expect(OngoingLeaderboard.second.score_secondary)
             .to eq(0.002) }
     end
 
@@ -139,32 +140,32 @@ describe CurrentOngoingLeaderboard do
     describe 'order by primary desc, secondary asc' do
       # Row | Submission | Participant | Primary | Secondary
       # ----|------------|-------------|---------|----------
-      #  1  |  p2s2      |  p2         |  36.00  | 0.003
+      #  1  |  p2s2      |  p2         |  40.00  | 0.005
       #  2  |  p1s5      |  p1         |  32.05  | 0.001
       before do
         challenge.update(
           primary_sort_order: :descending,
           secondary_sort_order: :ascending)
       end
-      it { expect(CurrentLeaderboard.count)
+      it { expect(OngoingLeaderboard.count)
             .to eq(2) }
 
-      it { expect(CurrentLeaderboard.first.participant_id)
+      it { expect(OngoingLeaderboard.first.participant_id)
             .to eq(participant2.id) }
-      it { expect(CurrentLeaderboard.first.row_num)
+      it { expect(OngoingLeaderboard.first.row_num)
             .to eq(1) }
-      it { expect(CurrentLeaderboard.first.score)
-            .to eq(36.00) }
-      it { expect(CurrentLeaderboard.first.score_secondary)
-            .to eq(0.003) }
+      it { expect(OngoingLeaderboard.first.score)
+            .to eq(40.00) }
+      it { expect(OngoingLeaderboard.first.score_secondary)
+            .to eq(0.005) }
 
-      it { expect(CurrentLeaderboard.second.participant_id)
+      it { expect(OngoingLeaderboard.second.participant_id)
             .to eq(participant1.id) }
-      it { expect(CurrentLeaderboard.second.row_num)
+      it { expect(OngoingLeaderboard.second.row_num)
             .to eq(2) }
-      it { expect(CurrentLeaderboard.second.score)
+      it { expect(OngoingLeaderboard.second.score)
             .to eq(32.05) }
-      it { expect(CurrentLeaderboard.second.score_secondary)
+      it { expect(OngoingLeaderboard.second.score_secondary)
             .to eq(0.001) }
     end
 
@@ -178,23 +179,23 @@ describe CurrentOngoingLeaderboard do
           primary_sort_order: :ascending,
           secondary_sort_order: :descending)
       end
-      it { expect(CurrentLeaderboard.count)
+      it { expect(OngoingLeaderboard.count)
             .to eq(2) }
-      it { expect(CurrentLeaderboard.first.participant_id)
+      it { expect(OngoingLeaderboard.first.participant_id)
             .to eq(participant1.id) }
-      it { expect(CurrentLeaderboard.first.row_num)
+      it { expect(OngoingLeaderboard.first.row_num)
             .to eq(1) }
-      it { expect(CurrentLeaderboard.first.score)
+      it { expect(OngoingLeaderboard.first.score)
             .to eq(30.05) }
-      it { expect(CurrentLeaderboard.first.score_secondary)
+      it { expect(OngoingLeaderboard.first.score_secondary)
             .to eq(0.002) }
-      it { expect(CurrentLeaderboard.second.participant_id)
+      it { expect(OngoingLeaderboard.second.participant_id)
             .to eq(participant2.id) }
-      it { expect(CurrentLeaderboard.second.row_num)
+      it { expect(OngoingLeaderboard.second.row_num)
             .to eq(2) }
-      it { expect(CurrentLeaderboard.second.score)
+      it { expect(OngoingLeaderboard.second.score)
             .to eq(31.00) }
-      it { expect(CurrentLeaderboard.second.score_secondary)
+      it { expect(OngoingLeaderboard.second.score_secondary)
             .to eq(0.003) }
     end
   end

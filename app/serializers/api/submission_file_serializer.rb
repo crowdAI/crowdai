16f798ba4df454db79466c15601e7da75a187a09
Sean Carroll <sfcarroll@gmail.com>
2018-04-06 12:58:22 +0200

    submissions endpoint

---
+++ app/serializers/api/submission_file_serializer.rb
@@ -0,0 +1,12 @@
+class Api::SubmissionFileSerializer < ActiveModel::Serializer
+  belongs_to :submission,
+    serializer: Api::SubmissionSerializer
+
+  attributes :id,
+    :submission_file_s3_key,
+    :aws_url
+
+  def aws_url
+    S3Service.new(object.submission_file_s3_key).expiring_url
+  end
+end
--- app/views/api/submissions/index.json.jbuilder
+++
@@ -1,6 +0,0 @@
-json.submissions do |json|
-  json.array! @submissions do |submission|
-    json.id submission.id
-    json.challenge_id submission.challenge_id
-  end
-end

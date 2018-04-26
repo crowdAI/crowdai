103d8d97a833bf82f82b3f8471c7d217ce5f5794
Sean Carroll <sfcarroll@gmail.com>
2018-04-26 15:28:34 +0200

    error handling for broken notebook urls

--- app/assets/javascripts/controllers/articles_controller.js
+++ app/assets/javascripts/controllers/articles_controller.js
@@ -2,12 +2,20 @@ Paloma.controller('Articles', {
   show: function(){
     var anchor = document.querySelector("#notebook-holder");
     var notebook_json = $('#notebook-data')[0].outerText;
-    var ipynb = JSON.parse(notebook_json)
-    var notebook = nb.parse(ipynb);
-    while (anchor.hasChildNodes()) {
-      anchor.removeChild(anchor.lastChild);
+    var ipynb = null;
+    try {
+      var ipynb = JSON.parse(notebook_json);
     }
-    anchor.appendChild(notebook.render());
-    Prism.highlightAll();
+    catch(error) {
+      console.log(error);
+    }
+    if(ipynb !== null){
+      var notebook = nb.parse(ipynb);
+      while (anchor.hasChildNodes()) {
+        anchor.removeChild(anchor.lastChild);
+      }
+      anchor.appendChild(notebook.render());
+      Prism.highlightAll();
+    }
   }
 });

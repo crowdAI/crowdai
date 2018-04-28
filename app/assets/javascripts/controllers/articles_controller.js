cffd5cf9e493b67fa52da780cc7bbc66afc10a32
SP Mohanty <spmohanty91@gmail.com>
2018-04-28 21:28:02 +0200

    Refactor javascript for conditional rendering of notebooks

--- app/assets/javascripts/controllers/articles_controller.js
+++ app/assets/javascripts/controllers/articles_controller.js
@@ -1,23 +1,23 @@
 Paloma.controller('Articles', {
   show: function(){
-    if ($("#notebook-data").length > 1){
-      var anchor = document.querySelector("#notebook-holder");
-      var notebook_json = $('#notebook-data')[0].outerText;
-      var ipynb = null;
-      try {
-        var ipynb = JSON.parse(notebook_json);
+    var anchor = document.querySelector("#notebook-holder");
+    var ipynb = null;
+    try {
+      if($('#notebook-data').length > 0){
+        var notebook_json = $('#notebook-data')[0].outerText;
+        ipynb = JSON.parse(notebook_json);
       }
-      catch(error) {
-        console.log(error);
+    }
+    catch(error) {
+      console.log(error);
+    }
+    if(ipynb !== null){
+      var notebook = nb.parse(ipynb);
+      while (anchor.hasChildNodes()) {
+        anchor.removeChild(anchor.lastChild);
       }
-      if(ipynb !== null){
-        var notebook = nb.parse(ipynb);
-        while (anchor.hasChildNodes()) {
-          anchor.removeChild(anchor.lastChild);
-        }
-        anchor.appendChild(notebook.render());
-        Prism.highlightAll();
-      }
+      anchor.appendChild(notebook.render());
+      Prism.highlightAll();
     }
   }
 });

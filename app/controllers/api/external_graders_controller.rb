628d3e286acfa1ce7067c65f975f2d0efd7e6640
SP Mohanty <spmohanty91@gmail.com>
2018-04-29 07:10:40 +0200

    Add a configurable parameter to overwwrite the meta object

--- app/controllers/api/external_graders_controller.rb
+++ app/controllers/api/external_graders_controller.rb
@@ -103,10 +103,19 @@ class Api::ExternalGradersController < Api::BaseController
         if submission.meta.nil?
           meta = params[:meta]
         else
-          # Standardise submission.meta to a Hash, irrespective of the
-          # version of the API
-          submission_meta = clean_meta(submission.meta)
-          meta = submission_meta.deep_merge(params[:meta])
+          if params[:meta_overwrite].present? && pararms[:meta_overwrite]
+            # When the provided parameters have a `meta_overwrite=True`
+            # parrameter, then a merge between the provided meta param
+            # and the submission meta param will *NOT* happen. In this
+            # case, the provided meta_param will overwrite the submission's
+            # meta param.
+            meta = params[:meta]
+          else
+            # Standardise submission.meta to a Hash, irrespective of the
+            # version of the API
+            submission_meta = clean_meta(submission.meta)
+            meta = submission_meta.deep_merge(params[:meta])
+          end
         end
         submission.update({meta: meta})
       end

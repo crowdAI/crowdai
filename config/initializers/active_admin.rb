bf4270fa60aeed6e9cee9c6cfb5b4d604d20c0f5
Sean Carroll <sfcarroll@gmail.com>
2018-05-01 17:35:22 +0200

    fix sign out link

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -104,7 +104,7 @@ ActiveAdmin.setup do |config|
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
-  # config.logout_link_method = :get
+  config.logout_link_method = :delete
 
   # == Root
   #

95ecdc006a8dfd63cd357a9fd1e0eee2848b6395
Sean Carroll <sfcarroll@gmail.com>
2018-05-03 16:08:29 +0200

    api

--- app/serializers/api/submission_file_serializer.rb
+++ app/serializers/api/submission_file_serializer.rb
@@ -4,7 +4,8 @@ class Api::SubmissionFileSerializer < ActiveModel::Serializer
 
   attributes :id,
     :submission_file_s3_key,
-    :aws_url
+    :aws_url,
+    :grading_status_cd
 
   def aws_url
     S3Service.new(object.submission_file_s3_key).expiring_url

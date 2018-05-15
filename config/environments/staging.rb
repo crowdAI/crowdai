8d3feeeac0942070f3bef56ad8ab90329ec98741
Sean Carroll <sfcarroll@gmail.com>
2018-05-15 16:02:37 +0200

    staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = true
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = :sass
   config.assets.compile = true
   config.assets.digest = true

342e7217bfd34edf6fd47c5eab32cdbdad669ce1
Sean Carroll <sfcarroll@gmail.com>
2018-06-05 14:55:09 +0200

    add fields for GDPR

--- config/initializers/gdpr.rb
+++ config/initializers/gdpr.rb
@@ -1,47 +1,38 @@
-=begin
-require 'gdpr_exporter'
-ActiveRecord::Base.send :include, GdprExporter
-
-Participant.gdpr_collect :email,
-  :created_at,
-  :updated_at,
-  :timezone,
-  :name,
-  :bio,
-  :website,
-  :github,
-  :linkedin,
-  :twitter,
-  :image_file,
-  :affiliation,
-  :country_cd,
-  :address,
-  :first_name,
-  :last_name,
-  {
-    user_id: :id, desc: "This is your basic information."
-  }
-=end
-
 GDPR_FIELDS = [
-  table: 'Participant',
-  id_field: 'id',
-  fields: [
-    :email,
-    :created_at,
-    :updated_at,
-    :timezone,
-    :name,
-    :bio,
-    :website,
-    :github,
-    :linkedin,
-    :twitter,
-    :image_file,
-    :affiliation,
-    :country_cd,
-    :address,
-    :first_name,
-    :last_name
-  ]
+  {
+    table: 'Participant',
+    id_field: 'id',
+    fields: [
+      :email,
+      :created_at,
+      :updated_at,
+      :timezone,
+      :name,
+      :bio,
+      :website,
+      :github,
+      :linkedin,
+      :twitter,
+      :image_file,
+      :affiliation,
+      :country_cd,
+      :address,
+      :first_name,
+      :last_name
+    ]
+  },
+  {
+    table: 'Topic',
+    id_field: 'participant_id',
+    fields: [
+      :topic
+    ]
+  },
+  {
+    table: 'Comment',
+    id_field: 'participant_id',
+    fields: [
+      :comment_markdown
+    ]
+  }
 ]

9fec8d66efe4cae469ff93abeff0022ca4ef1238
Sean Carroll <sfcarroll@gmail.com>
2018-06-07 10:27:31 +0200

    temporary workaround

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  #protect_from_forgery
   #protect_from_forgery with: :exception, prepend: true
+  skip_before_action :verify_authenticity_token, raise: false
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized_or_login
   after_action :participant_activity

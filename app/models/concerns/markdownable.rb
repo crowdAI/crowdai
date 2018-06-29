a12cbb9dbf5347b6864356386ce95908905a8bd3
Sean Carroll <sfcarroll@gmail.com>
2018-06-29 11:13:03 +0200

    refactoring

--- app/models/concerns/markdownable.rb
+++ app/models/concerns/markdownable.rb
@@ -10,7 +10,7 @@ module Markdownable
     self.changed_attribute_names_to_save.select{|c| c.end_with?("markdown") }.each do |col_markdown|
       col = col_markdown.remove("_markdown")
       self.send("#{col}=", Kramdown::Document.new(
-        self.send(col_markdown),{coderay_line_numbers: nil}).to_html)
+        self.send(col_markdown),{coderay_line_numbers: nil, syntax_highlighter: 'rouge'}).to_html)
     end
   end
 

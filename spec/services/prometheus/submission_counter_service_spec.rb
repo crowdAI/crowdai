e60f32062ae584c49b1a2c188bdad36334280dc8
Sean Carroll <sfcarroll@gmail.com>
2018-07-31 09:56:22 +0200

    secure prometheus endpoint

--- spec/services/prometheus/submission_counter_service_spec.rb
+++ spec/services/prometheus/submission_counter_service_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Prometheus::SubmissionCounterService do
   let(:submission) { create :submission }
 
-  describe '#call' do
-    #it do
-    #  expect {
-    #    described_class.new(submission_id: submission.id).call
-    #  }.to change{EventLog.count}.by(1)
-    #end
-  end
+  #describe '#call' do
+  #  it do
+  #    expect {
+  #      described_class.new(submission_id: #s#ubmission.id).call
+  #    }.to change{EventLog.count}.by(1)
+  #  end
+  #end
 
 end

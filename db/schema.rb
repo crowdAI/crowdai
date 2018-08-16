4081a38a488ec232aea2fef707f1475c431b42ce
Sean Carroll <sfcarroll@gmail.com>
2018-08-16 16:02:19 +0200

    run recalc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_115657) do
+ActiveRecord::Schema.define(version: 2018_08_16_140013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"

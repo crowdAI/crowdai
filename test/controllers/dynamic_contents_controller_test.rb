458bfd5dfd0ff34dae2ece21a805e3c2e3ac3551
Sean <sfcarroll@gmail.com>
2018-10-08 16:57:33 +0200

    test refactoring

---
+++ test/controllers/dynamic_contents_controller_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class DynamicContentsControllerTest < ActionDispatch::IntegrationTest
+  
+  end
---
+++ test/controllers/short_urls_controller_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class MembersControllerTest < ActionDispatch::IntegrationTest
+
+  end

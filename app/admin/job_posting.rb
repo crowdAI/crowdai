7085bf18a6fb5bdd338be8768ce49f3c01d0e2bc
Sean <sfcarroll@gmail.com>
2018-10-11 16:58:28 +0200

    jobs

--- app/admin/job_posting.rb
+++ app/admin/job_posting.rb
@@ -24,5 +24,42 @@ ActiveAdmin.register JobPosting do
   filter :remote
   filter :posting_date
   filter :closing_date
+  filter :status
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :status,
+        as: :select,
+        collection:  enum_option_pairs(JobPosting, :status),
+        include_blank: false
+      f.input :posting_date
+      f.input :closing_date
+      f.input :location
+      f.input :country
+      f.input :remote
+      f.input :description
+    end
+    f.actions
+  end
+
+  show do |job|
+    attributes_table do
+      row :title
+      row :status
+      row :posting_date
+      row :closing_date
+      row :location
+      row :country
+      row :remote
+      row :description do
+        sanitize(job.description)
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
+
+
 
 end

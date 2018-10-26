1b1007cc5271b4bef3d3b99601a73e86305ec45f
Sean <sfcarroll@gmail.com>
2018-10-26 17:43:33 +0200

    handle empty params

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -2,9 +2,12 @@ class CustomFailure < Devise::FailureApp
   include ActionView::Helpers::UrlHelper
 
   def redirect_url
-    participant = Participant.find_by(email: params[:participant][:email])
-    if participant.present? && participant.confirmed_at.blank?
-      flash[:alert] = "Your account has not yet been confirmed. Please check your email and confirm your account or #{link_to 'request a new confirmation link.', new_participant_confirmation_path}".html_safe
+    if params[:participant].present?
+      participant = Participant.find_by(
+        email: params[:participant][:email])
+      if participant.present? && participant.confirmed_at.blank?
+        flash[:alert] = "Your account has not yet been confirmed. Please check your email and confirm your account or #{link_to 'request a new confirmation link.', new_participant_confirmation_path}".html_safe
+      end
     end
     new_participant_session_url(:subdomain => 'secure')
    end
